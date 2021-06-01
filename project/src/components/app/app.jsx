d3e553a98897ce7667f6388ae8c8dc32c36ea1a5
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-01 16:47:54 -0700

    Wrap place cards with react component

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import MainPage from '../main-page/main-page.jsx';
 
-function App() {
+export default function App() {
   return <MainPage/>;
 }
-
-export default App;
---
+++ project/src/data.js
@@ -0,0 +1,37 @@
+export const data = {
+  cards: [{
+    label: 'Premium',
+    image: '../img/apartment-01.jpg',
+    price: '120',
+    name: 'Beautiful & luxurious apartment at great location',
+    type: 'Apartment',
+    rating: 100,
+  },{
+    image: '../img/room.jpg',
+    price: '80',
+    name: 'Wood and stone place',
+    type: 'Private room',
+    rating: 80,
+    saved: true,
+  },{
+    image: '../img/apartment-02.jpg',
+    price: '132',
+    name: 'Canal View Prinsengracht',
+    type: 'Apartment',
+    rating: 60,
+  },{
+    label: 'Premium',
+    image: '../img/apartment-03.jpg',
+    price: '180',
+    name: 'Nice, cozy, warm big bed apartment',
+    type: 'Apartment',
+    rating: 50,
+  },{
+    image: '../img/room.jpg',
+    price: '80',
+    name: 'Wood and stone place',
+    type: 'Private room',
+    rating: 100,
+    saved: true,
+  }],
+};
