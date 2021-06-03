e8e556ae325e9a21d6f05fa919875fb530a1869e
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-03 15:59:08 -0700

    Add basic routing

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,18 +1,34 @@
 import React from 'react';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
 import MainPage from '../main-page/main-page.jsx';
 import PropTypes from 'prop-types';
 import FavoritesPage from '../favorites-page/favorites-page';
 import LoginPage from '../login-page/login-page';
 import NotFoundPage from '../not-found-page/not-found-page';
 
+const routes = {
+  Main: '/',
+  Login: '/login',
+  Favorites: '/favorites',
+};
 export default function App(props) {
   return (
-    <>
-      <NotFoundPage/>
-      <LoginPage/>
-      <FavoritesPage/>
-      <MainPage settings={props.settings} data={props.data}/>
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={routes.Main}>
+          <MainPage settings={props.settings} data={props.data}/>
+        </Route>
+        <Route exact path={routes.Login}>
+          <LoginPage/>
+        </Route>
+        <Route exact path={routes.Favorites}>
+          <FavoritesPage/>
+        </Route>
+        <Route>
+          <NotFoundPage/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
