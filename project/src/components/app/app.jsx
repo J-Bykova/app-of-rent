f7ddc1d72e8edec658afb8c2647ebd2fd4717ad6
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-03 15:38:03 -0700

    Rewrite pages in jsx

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import MainPage from '../main-page/main-page.jsx';
 import PropTypes from 'prop-types';
+import FavoritesPage from '../favorites-page/favorites-page';
+import LoginPage from '../login-page/login-page';
 
 export default function App(props) {
-  return <MainPage settings={props.settings} data={props.data} />;
+  return (
+    <>
+      <LoginPage/>
+      <FavoritesPage/>
+      <MainPage settings={props.settings} data={props.data}/>
+    </>
+  );
 }
 
 App.propTypes = {
--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -1,44 +1,12 @@
 import React from 'react';
 import PlaceCard from '../place-card/place-card';
 import PropTypes from 'prop-types';
+import HeaderSection from '../header-section/header-section';
 
 export default function MainPage(props) {
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active" href="#">
-                <img
-                  className="header__logo"
-                  src="../img/logo.svg"
-                  alt="6 cities logo"
-                  width="81"
-                  height="41"
-                />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">{props.data.header.email}</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <HeaderSection/>
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -84,8 +52,7 @@ export default function MainPage(props) {
               <b className="places__found">312 places to stay in Amsterdam</b>
               <form className="places__sorting" action="#" method="get">
                 <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
+                <span className="places__sorting-type" tabIndex="0">Popular
                   <svg className="places__sorting-arrow" width="7" height="4">
                     <use xlinkHref="#icon-arrow-select"></use>
                   </svg>
