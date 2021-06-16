1b22d714514da993c7098c42d34a76b14ff82c51
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-16 14:27:49 -0700

    Replace static offers with dynamic data

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -65,7 +65,7 @@ export default function MainPage(props) {
                 </ul>
               </form>
               <div className="cities__places-list places__list tabs__content">
-                {props.offers.slice(0, props.settings.CARDS_COUNT).map((card) => (
+                {props.offers.map((card) => (
                   <PlaceCard key={card.id} settings={props.settings} routes={props.routes} {...card}/>
                 ))}
               </div>
--- project/src/index.js
+++ project/src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
-import {offers, loadOffers} from './mocks/offers';
+import {loadOffers} from './mocks/offers';
 import {settings} from './settings';
 import {routes} from './routes';
 
-loadOffers(1).then((res) => {
-  // eslint-disable-next-line no-console
-  console.log(res);
-
+loadOffers(settings.CARDS_COUNT).then((offers) => {
   ReactDOM.render(
     <React.StrictMode>
       <App
