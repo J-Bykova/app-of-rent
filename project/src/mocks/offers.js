c0f9f7370ac1ae90ce2f59f67219445ce5f47664
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-16 14:22:00 -0700

    Create a data mapper for offers

--- project/src/mocks/offers.js
+++ project/src/mocks/offers.js
@@ -2,8 +2,23 @@
 export function loadOffers(limit) {
   return fetch('https://7.react.pages.academy/six-cities/hotels')
     .then((response) => response.json())
-    .then((json) => json.slice(0, limit));
+    .then((json) => json.slice(0, limit))
+    .then((data) => data.map(mapDataToClientModel));
 }
+
+function mapDataToClientModel(data) {
+  return {
+    id: data.id,
+    isPremium: data.is_premium,
+    previewImage: data.preview_image,
+    price: data.price,
+    title: data.title,
+    type: data.type,
+    isFavorite: data.is_favorite,
+    rating: data.rating,
+  };
+}
+
 
 export const offers = [{
   id: 1,
