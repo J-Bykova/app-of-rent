0d7e5c71ba324dd658e18794e14d880323186cda
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-06-16 14:03:02 -0700

    Replace the hardcoded types with a dictionary

--- project/src/mocks/offers.js
+++ project/src/mocks/offers.js
@@ -11,7 +11,7 @@ export const offers = [{
   previewImage: '../img/apartment-01.jpg',
   price: 120,
   title: 'Beautiful & luxurious apartment at great location',
-  type: 'Apartment',
+  type: 'apartment',
   rating: 100,
 }, {
   id: 2,
@@ -19,7 +19,7 @@ export const offers = [{
   previewImage: '../img/room.jpg',
   price: 80,
   title: 'Wood and stone place',
-  type: 'Private room',
+  type: 'room',
   rating: 80,
   saved: true,
 }, {
@@ -28,7 +28,7 @@ export const offers = [{
   previewImage: '../img/apartment-02.jpg',
   price: 132,
   title: 'Canal View Prinsengracht',
-  type: 'Apartment',
+  type: 'apartment',
   rating: 60,
 }, {
   id: 4,
@@ -36,7 +36,7 @@ export const offers = [{
   previewImage: '../img/apartment-03.jpg',
   price: 180,
   title: 'Nice, cozy, warm big bed apartment',
-  type: 'Apartment',
+  type: 'apartment',
   rating: 50,
 }, {
   id: 5,
@@ -44,7 +44,7 @@ export const offers = [{
   previewImage: '../img/room.jpg',
   price: 80,
   title: 'Wood and stone place',
-  type: 'Private room',
+  type: 'room',
   rating: 100,
   saved: true,
 }];
