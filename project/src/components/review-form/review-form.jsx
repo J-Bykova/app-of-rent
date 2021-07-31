27ba0cbc07bad12d926995a7e6fe874256a4b8ad
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-31 10:19:53 -0700

    Update ReviewForm

--- project/src/components/review-form/review-form.jsx
+++ project/src/components/review-form/review-form.jsx
@@ -1,37 +1,72 @@
 import React from 'react';
 
-const stars = [{
-  id: '5-stars',
-  value: 5,
-  title: 'perfect',
-},{
-  id: '4-stars',
-  value: 4,
-  title: 'good',
-},{
-  id: '3-stars',
-  value: 3,
-  title: 'not bad',
-},{
-  id: '2-stars',
-  value: 2,
-  title: 'badly',
-},{
-  id: '1-star',
-  value: 1,
-  title: 'terribly',
-}];
+const stars = [
+  {
+    id: '5-stars',
+    value: 5,
+    title: 'perfect',
+  }, {
+    id: '4-stars',
+    value: 4,
+    title: 'good',
+  }, {
+    id: '3-stars',
+    value: 3,
+    title: 'not bad',
+  }, {
+    id: '2-stars',
+    value: 2,
+    title: 'badly',
+  }, {
+    id: '1-star',
+    value: 1,
+    title: 'terribly',
+  }];
 
 export default class ReviewForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      rating: '',
+      review: '',
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleFieldChange = this.handleFieldChange.bind(this);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+  }
+
+  handleFieldChange(evt) {
+    const {name, value} = evt.target;
+    this.setState({[name]: value});
+  }
+
   render() {
     return (
-      <form className="reviews__form form" action="#" method="post">
+      <form className="reviews__form form" action="#" method="post" onSubmit={this.handleSubmit}>
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
           {stars.map((star) => (
             <>
-              <input className="form__rating-input visually-hidden" name="rating" value={star.value} id={star.id} type="radio"/>
-              <label htmlFor={star.id} className="reviews__rating-label form__rating-label" title={star.title}>
+              <input
+                className="form__rating-input visually-hidden"
+                name="rating"
+                value={star.value}
+                id={star.id}
+                type="radio"
+                key={star.value}
+                onChange={this.handleFieldChange}
+              />
+              <label
+                className="reviews__rating-label form__rating-label"
+                htmlFor={star.id}
+                title={star.title}
+                key={star.value}
+              >
                 <svg className="form__star-image" width="37" height="33">
                   <use xlinkHref="#icon-star"></use>
                 </svg>
@@ -39,11 +74,19 @@ export default class ReviewForm extends React.Component {
             </>
           ))}
         </div>
-        <textarea className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+        <textarea
+          className="reviews__textarea form__textarea"
+          id="review"
+          name="review"
+          placeholder="Tell how was your stay, what you like and what can be improved"
+          onChange={this.handleFieldChange}
+        />
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
-          To submit review please make sure to set <span className="reviews__star">rating</span> and
-          describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
+            To submit review please make sure to set
+            <span className="reviews__star">rating</span>
+            and describe your stay with at least
+            <b className="reviews__text-amount">50 characters</b>.
           </p>
           <button className="reviews__submit form__submit button" type="submit" disabled="">Submit</button>
         </div>
@@ -51,3 +94,4 @@ export default class ReviewForm extends React.Component {
     );
   }
 }
+
