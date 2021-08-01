74c425401ff4e43d20bf3bed4a8b1857ac9669b0
Jenny Bykova <eva.gre4ka@yandex.ru>
2021-07-31 18:02:15 -0700

    Refactoring  ReviewForm

--- project/src/components/review-form/review-form.jsx
+++ project/src/components/review-form/review-form.jsx
@@ -1,27 +1,5 @@
 import React from 'react';
 
-const stars = [{
-  id: '5-stars',
-  value: 5,
-  title: 'perfect',
-}, {
-  id: '4-stars',
-  value: 4,
-  title: 'good',
-}, {
-  id: '3-stars',
-  value: 3,
-  title: 'not bad',
-}, {
-  id: '2-stars',
-  value: 2,
-  title: 'badly',
-}, {
-  id: '1-star',
-  value: 1,
-  title: 'terribly',
-}];
-
 export default class ReviewForm extends React.Component {
   constructor(props) {
     super(props);
@@ -91,4 +69,26 @@ export default class ReviewForm extends React.Component {
     );
   }
 }
+
+const stars = [{
+  id: '5-stars',
+  value: 5,
+  title: 'perfect',
+}, {
+  id: '4-stars',
+  value: 4,
+  title: 'good',
+}, {
+  id: '3-stars',
+  value: 3,
+  title: 'not bad',
+}, {
+  id: '2-stars',
+  value: 2,
+  title: 'badly',
+}, {
+  id: '1-star',
+  value: 1,
+  title: 'terribly',
+}];
 
